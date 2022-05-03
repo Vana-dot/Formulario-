23d43e251bfe0e670b436f407902faba37e5270b
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-05-02 23:44:57 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function myname(){
+    //let nome = document.querySelector("#name").innerHTML="Geovana submetida com sucesso";    
+    let nome = document.getElementById('name').value;
+    alert(`Usuário: ${nome} submetido com sucesso`);
+}
+
